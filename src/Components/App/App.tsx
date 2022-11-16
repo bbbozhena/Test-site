f0720eacfd548258ae090902933435d38a325e97
bbbozhena <bbbozhenka@gmail.com>
2022-11-16 19:49:53 +0200

    fix: receiving data and add them to component

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.scss";
 import JobBar from "../JobBoard/JobBar/JobBar.tsx";
-import GotServices from "../Services/GotServices";
+import axios from "axios";
 
 function App() {
+  const apiUrl = "https://api.json-generator.com/";
+  const apiToken = "wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu";
+
+  const [inf, setInf] = useState([]);
+
+  useEffect(() => {
+    const instance = axios.create({
+      baseURL: apiUrl,
+      timeout: 1000,
+      headers: { Authorization: "Bearer " + apiToken },
+    });
+
+    instance
+      .get("templates/ZM1r0eic3XEy/data")
+      .then((response) => {
+        setInf(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <JobBar />
-      <GotServices />
+      {inf.map((i, id) => {
+        return <JobBar item={i} key={id} />;
+      })}
     </div>
   );
 }
